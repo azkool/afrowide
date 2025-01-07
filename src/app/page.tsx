0354eba092fdb9897859b9609be31f82e71d4760
azkool <87613755+azkool@users.noreply.github.com>
2025-01-07 12:29:54 +0000

    navbar first work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-
+import { Navbar } from "@/ui/components/Navbar"
 const Home = () => {
 
     return (
-        <div className='flex justify-center items-center h-screen'>
-            <h1 className='text-3xl font-bold underline'>Afrowide</h1>
-        </div>
+        <>
+           <Navbar />
+            <div className='flex justify-center items-center h-screen'>
+                <h1 className='text-3xl font-bold underline'>Afrowide</h1>
+            </div>
+        </>
+        
+        
+        
+
+        
     )
 }
 
---
+++ src/ui/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { IoMenuSharp, IoSearch } from 'react-icons/io5'
+import Link from 'next/link'
+export function Navbar() {
+  return (
+    <div className="flex justify-between items-center h-16 bg-white text-white px-8">
+      <div>
+        <Link href="/" className='text-black'>afrowide</Link>
+      </div>
+      <div className='flex gap-4 text-black'>
+      <IoSearch />
+      <IoMenuSharp />
+      </div>
+    </div>
+  )
+}
